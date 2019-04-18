1c73b1ff861c8f2605e87c60d7fed3e1c0457c24
Marcosvpj <marcos@marcosvpj.com.br>
2019-04-17 23:00:29 -0300

    Add variables to interperter

--- Interpreter.py
+++ Interpreter.py
@@ -1,6 +1,15 @@
 class Interpreter:
     def __init__(self):
         self.stack = []
+        self.enviroment = {}
+
+    def STORE_NAME(self, name):
+        value = self.stack.pop()
+        self.enviroment[name] = value
+
+    def LOAD_NAME(self, name):
+        value = self.enviroment[name]
+        self.stack.append(value)
 
     def LOAD_VALUE(self, number):
         self.stack.append(number)
@@ -15,16 +24,30 @@ class Interpreter:
         result = first + second
         self.stack.append(result)
 
+    def parse_argument(self, instruction, argument, what_to_execute):
+        """ Return the right arguments for each type of instruction """
+        numbers = ['LOAD_VALUE']
+        names = ['STORE_NAME', 'LOAD_NAME']
+
+        if instruction in numbers:
+            return what_to_execute['numbers'][argument]
+        elif instruction in names:
+            return what_to_execute['names'][argument]
+
     def run_code(self, what_to_execute):
         instructions = what_to_execute['instructions']
-        numbers = what_to_execute['numbers']
 
         for each_step in instructions:
             instruction, argument = each_step
+            argument = self.parse_argument(
+                instruction, argument, what_to_execute)
             if instruction == 'LOAD_VALUE':
-                number = numbers[argument]
-                self.LOAD_VALUE(number)
+                self.LOAD_VALUE(argument)
             elif instruction == 'PRINT_ANSWER':
                 self.PRINT_ANSWER()
             elif instruction == 'ADD_TWO_VALUES':
                 self.ADD_TWO_VALUES()
+            elif instruction == 'STORE_NAME':
+                self.STORE_NAME(argument)
+            elif instruction == 'LOAD_NAME':
+                self.LOAD_NAME(argument)
--- main.py
+++ main.py
@@ -4,11 +4,16 @@ from Interpreter import Interpreter
 instructions = {
     'instructions': [
         ['LOAD_VALUE', 0],
+        ['STORE_NAME', 0],
         ['LOAD_VALUE', 1],
+        ['STORE_NAME', 1],
+        ['LOAD_NAME', 0],
+        ['LOAD_NAME', 1],
         ['ADD_TWO_VALUES', None],
         ['PRINT_ANSWER', None],
     ],
-    'numbers': [2, 3]
+    'numbers': [2, 3],
+    'names': ['a', 'b'],
 }
 
 i = Interpreter()
