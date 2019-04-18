d92b165c62ab4c75e8ca8bf287cd8c2a153827bb
Marcosvpj <marcos@marcosvpj.com.br>
2019-04-17 22:48:46 -0300

    Refactor: Organize what will run and Interpreter in separated files

--- main.py
+++ main.py
@@ -1,33 +1,4 @@
-class Interpreter:
-    def __init__(self):
-        self.stack = []
-
-    def LOAD_VALUE(self, number):
-        self.stack.append(number)
-
-    def PRINT_ANSWER(self):
-        value = self.stack.pop()
-        print(value)
-
-    def ADD_TWO_VALUES(self):
-        first = self.stack.pop()
-        second = self.stack.pop()
-        result = first + second
-        self.stack.append(result)
-
-    def run_code(self, what_to_execute):
-        instructions = what_to_execute['instructions']
-        numbers = what_to_execute['numbers']
-
-        for each_step in instructions:
-            instruction, argument = each_step
-            if instruction == 'LOAD_VALUE':
-                number = numbers[argument]
-                self.LOAD_VALUE(number)
-            elif instruction == 'PRINT_ANSWER':
-                self.PRINT_ANSWER()
-            elif instruction == 'ADD_TWO_VALUES':
-                self.ADD_TWO_VALUES()
+from Interpreter import Interpreter
 
 
 instructions = {
